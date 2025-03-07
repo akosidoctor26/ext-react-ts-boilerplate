384eff17da3bc71a0f48042d7a195e9a658b649e
Kristine Doc <mskristinedoctor@gmail.com>
2025-03-06 17:56:50 -0700

    Organize files

--- src/app/options/OptionsIndex.tsx
+++ src/app/options/OptionsIndex.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import Options from './components/Options';
+import Options from './Options';
 
 const root = createRoot(document.getElementById('optionsRoot') as HTMLElement);
 root.render(<Options />);
--- src/app/popup/PopupIndex.tsx
+++ src/app/popup/PopupIndex.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import Popup from './components/Popup';
+import Popup from './Popup';
 
 const root = createRoot(document.getElementById('popupRoot') as HTMLElement);
 root.render(<Popup />);
